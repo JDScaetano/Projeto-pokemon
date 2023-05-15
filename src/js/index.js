146d64933d2ec85b1ce62dbcc85c9fda11408638
Jadson Caetano <103514803+JDScaetano@users.noreply.github.com>
2023-05-15 14:59:51 -0400

    atualização

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+
+const botaoTema = document.getElementById("botao-tema"); 
+const body = document.querySelector("body") 
+const trocaBotao = document.querySelector(".imagem-botao")
+
+botaoTema.addEventListener("click", () => { 
+    const modoEscuroAtivo = body.classList.contains("modo-escuro")
+    
+    if (modoEscuroAtivo){ 
+        body.classList.remove("modo-escuro")
+        trocaBotao.setAttribute("src", "/src/imagens/sun.png")
+    } else { 
+        body.classList.add("modo-escuro")
+        trocaBotao.setAttribute("src", "/src/imagens/moon.png")
+    } 
+})
+j
+
+
